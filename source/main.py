91451a40dfdf6e384dc1b9c74f5197edee8d2648
Erik Belluomini <erikbelluomini@gmail.com>
2021-04-28 06:18:36 -0500

    Update main.py
    Updated for new servercheck command using nmap.
    Added tarhelp command

--- source/main.py
+++ source/main.py
@@ -14,7 +14,11 @@ makeFileObj = open("Makefile.txt", "r")
 makeFile = makeFileObj.read()
 makeFileObj.close()
 
-TOKEN = #Token goes here
+tarHelpObj = open("TarHelp.txt", "r")
+tarHelp = tarHelpObj.read()
+tarHelpObj.close()
+
+TOKEN = #Token Goes here
 
 bot = commands.Bot(command_prefix='=')
 
@@ -23,35 +27,41 @@ async def on_ready():
     print(f'The bot has connected to Discord!')
 
 @bot.command(name='servercheck', help='Checks the cycle servers to see if they are up')
-async def checkServers(ctx):
-    #This is commented out since the EECS servers do not appreciate pings.
-    #param = '-n' if platform.system().lower()=='windows' else '-c'
+async def checkServers(ctx): 
+    commandCycle2 = ['nmap', '-p22', "cycle2.eecs.ku.edu"]
+    cycle2up = subprocess.run(commandCycle2, capture_output=True)
+    
+    cycle2Output = str(cycle2up.stdout)
+    cycle2HostUp = cycle2Output.find('Host is up') != -1
+    cycle2Open = cycle2Output.find('tcp open  ssh') != -1
 
-    # Building the command. Ex: "ping -c 1 google.com"
-    #commandCycle2 = ['ping', param, '1', "cycle2.eecs.ku.edu"]
-    #cycle2up = subprocess.call(commandCycle2) == 0
-
-    #commandCycle3 = ['ping', param, '1', "cycle3.eecs.ku.edu"]
-    #cycle3up = subprocess.call(commandCycle3) == 0
-
-    #await ctx.send('Cycle2 Servers are up: ' + str(cycle2up) + '\nCycle3 Servers are up: ' + str(cycle3up))
-    await ctx.send('For now, the cycle servers seem to be unable to be pinged. They may reject this protocol on the servers.')
-
+    commandCycle3 = ['nmap', '-p22', "cycle3.eecs.ku.edu"]
+    cycle3up = subprocess.run(commandCycle3, capture_output=True)
+    
+    cycle3Output = str(cycle3up.stdout)
+    cycle3HostUp = cycle3Output.find('Host is up') != -1
+    cycle3Open = cycle3Output.find('tcp open  ssh') != -1
+    
+    await ctx.send('Cycle2 Servers are up: ' + str(cycle2HostUp) +  ', SSH Connection up: ' + str(cycle2Open) + '\nCycle3 Servers are up: ' + str(cycle3HostUp) + ', SSH Connection up: ' + str(cycle3Open))
 
 @bot.command(name='makefile', help='Prints a makefile that is supplied by the 268 wiki')
 async def printMakeFile(ctx):
     await ctx.send(makeFile)
 
-@bot.command(name='link', help='Links a class wiki page. Syntax is =link [Class#]. Example: -link 168.')
+@bot.command(name='link', help='Links a class wiki page. Syntax is =link [Class#]')
 async def sendLink(ctx, classNumber):
     await ctx.send('https://wiki.ittc.ku.edu/ittc_wiki/index.php/EECS' + str(classNumber))
 
 @bot.command(name='classList', help='Prints out the class list on the EECS wiki')
 async def printClassList(ctx):
     await ctx.send(classList)
-    
+
 @bot.command(name='github', help='Prints out the github link for this project')
 async def printGitHub(ctx):
     await ctx.send('https://github.com/DeadlyForce1214/KU-EECS-Bot')
+
+@bot.command(name='tarhelp', help='Prints out useful tar information')
+async def printTarCommands(ctx):
+    await ctx.send(tarHelp)
 
 bot.run(TOKEN)
